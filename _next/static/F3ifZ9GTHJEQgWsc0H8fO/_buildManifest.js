03553a99261a7a5d819ed522dc70ba444b3b12a2
iuemon83 <iuemon83@users.noreply.github.com>
2021-05-27 16:04:57 +0000

    deploy: fb52cbcf909bd4fcbdeabd3ddfab34a0c05db8cf

---
+++ _next/static/F3ifZ9GTHJEQgWsc0H8fO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-be0c58dbccb4e9d78d62.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa0fefe1ec7a11692575.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
